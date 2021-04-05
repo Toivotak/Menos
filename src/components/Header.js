fb8ce460e735d8a192ca3fc9988a0a334a3ff72b
Toivotak <50209071+Toivotak@users.noreply.github.com>
2021-04-05 19:49:34 +0300

    small changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,10 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("clikkatu")
-    }
 
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button color="green" text="Add" onClick={onClick}/>
         </header>
     )
 }

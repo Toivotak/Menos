d4839731bf43d7181deb15207328784f77ebc005
Toivotak <50209071+Toivotak@users.noreply.github.com>
2021-04-09 16:48:39 +0300

    hook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,30 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-const Button = ({color, text, onClick}) => {
+const Button = ({type, color, text, onClick}) => {
+    const [count, setCount] = useState(1);
+    let btns = document.getElementsByClassName("btn");
+    if (count % 71 === 0){
+        color = "orange";
+    }
+
     return <button 
-        onClick={onClick}
+        type={type}
+        onClick={onClick, () => setCount(count + 1)}
         style={{backgroundColor : color}} 
-        className='btn'>
+        className="btn"> 
             {text}
         </button>
 }
 
 Button.defaultProps = {
+    type: "button",
     color: "red",
     text: "Missing"
 }
 
 Button.propTypes = {
+    type: PropTypes.string.isRequired,
     text: PropTypes.string,
     color: PropTypes.string,
     onClick: PropTypes.func

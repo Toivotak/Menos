4429f0a0b2462310985fb35f8965e42d00c3087a
Toivotak <50209071+Toivotak@users.noreply.github.com>
2021-05-31 13:30:08 +0300

    stars

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = ({ title }) => {
     return (
         <header className="header">
             <div className="moon-left" />
+            <div className="star-left"><div id="star-inner-left"/></div>
             <h1>{title}</h1>
+            <div className="star-right"><div id="star-inner-right"/></div>
             <div className="moon-right" />
         </header>
     )
